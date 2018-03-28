721e55df878d1e9c3e54deb35f7e76b2fd1aa6c4
simba <1531315@qq.com>
2018-03-28 16:55:49 +0800

    修改使用代理的例子

--- example/proxy_demo.py
+++ example/proxy_demo.py
@@ -2,6 +2,7 @@
 使用代理设置
 """
 import re
+from time import time
 
 from translateyoudao.translate import translate
 from translateyoudao.proxy import get_proxies
@@ -31,11 +32,26 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """
-words = re.findall(p, text)
+words = set(re.findall(p, text))
 
 # 输入你购买的代理订单号
-order_no = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+order_no = '28f69ed72e7024d8fe1988d4bf6f8e54'
 # 获得代理对象，只需获取一次代理对象就行了，它会自动实现每隔5秒刷新一次代理IP
 proxies = get_proxies(get_proxy_ip, args=(order_no, ))
 # 建议一次不要传入太多单词
-print(translate(*words, proxies=proxies))
+begin = time()
+res = translate(*words, proxies=proxies)
+end = time()
+print('请求接口走了代理，以及有重试机制，所以速度稍慢')
+print('用时: {}秒'.format(int(end - begin)))
+success = 0
+failed = 0
+print('单词数: {}个'.format(len(words)))
+for v in res.values():
+    if v is not False:
+        success += 1
+    else:
+        failed += 1
+
+print('成功: {}个'.format(success))
+print('失败: {}个'.format(failed))
