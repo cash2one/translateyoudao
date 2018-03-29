a1dc6858ba898441c49bf9707692826dd491d085
simba <1531315@qq.com>
2018-03-29 14:13:19 +0800

    切换代理IP函数改用代理IP对象的弱引用，防止内存泄露

--- translateyoudao/proxy.py
+++ translateyoudao/proxy.py
@@ -1,5 +1,11 @@
+import weakref
 from threading import Thread
 from time import sleep
+
+
+class ProxiesDict(dict):
+    """方便弱引用"""
+    pass
 
 
 def flush_proxies(func, args, proxies, sleep_time):
@@ -14,7 +20,11 @@ def flush_proxies(func, args, proxies, sleep_time):
     while True:
         sleep(sleep_time)
         proxy_ip = func(*args)
-        proxies['https'] = proxies['http'] = proxy_ip
+        try:
+            proxies['https'] = proxies['http'] = proxy_ip
+        # 代理IP字典对象已被销毁，终止切换代理IP
+        except ReferenceError:
+            break
 
 
 def get_proxies(func, args=None, sleep_time=5):
@@ -31,11 +41,10 @@ def get_proxies(func, args=None, sleep_time=5):
         args = ()
 
     proxy_ip = func(*args)
-    proxies = {
-        'http': proxy_ip,
-        'https': proxy_ip,
-    }
-    t1 = Thread(target=flush_proxies, args=(func, args, proxies, sleep_time), daemon=True)
+    proxies = ProxiesDict(http=proxy_ip, https=proxy_ip)
+    # 使用弱引用代理对象，防止内存泄露
+    proxies_proxy = weakref.proxy(proxies)
+    t1 = Thread(target=flush_proxies, args=(func, args, proxies_proxy, sleep_time), daemon=True)
     t1.start()
 
     return proxies
